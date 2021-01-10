1a5aeba3cf9ef34aeb6ccbbda92fce6bb6a04c4b
vincenzo-petronio <vincenzo.petronio@gmail.com>
2021-01-10 10:30:58 +0100

    FIX

--- AppTicket/ticket.tickets/src/test/java/it/localhost/learningspring/ticket/tickets/ApplicationTests.java
+++ AppTicket/ticket.tickets/src/test/java/it/localhost/learningspring/ticket/tickets/ApplicationTests.java
@@ -1,13 +1,13 @@
-package it.localhost.learningspring.ticket.tickets;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package it.localhost.learningspring.ticket.tickets;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
